f0a8e2b919abf88524aeb03032451debec7ff874
Daniel Hoffmann <d.hoffmann@metaways.de>
2022-09-21 16:27:49 +0200

    Remove obsolete local build scripts

--- build/local_docker_build_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-scriptDir=$(dirname "$0")
-projectRoot=${scriptDir}/..
-
-. ${projectRoot}/build/local_tag.sh
-
-docker build -t dockerregistry.metaways.net/tine20/docker/tine20-broadcasthub:$tag $projectRoot
