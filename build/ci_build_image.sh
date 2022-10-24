54fcff1ab265cce92498ab0cbacb8bd1d65cc191
Daniel Hoffmann <d.hoffmann@metaways.de>
2022-10-24 16:31:06 +0200

    Enable docker multi-arch image builds for amd and arm
    https://taiga.metaways.net/project/admin-tine20-service/us/4240

--- build/ci_build_image.sh
+++ build/ci_build_image.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 
-docker build -t dockerregistry.metaways.net/tine20/tine20-broadcasthub:$CI_COMMIT_TAG -t registry.metaways.net/tine/broadcasthub:$CI_COMMIT_TAG -t docker.io/tinegroupware/broadcasthub:$CI_COMMIT_TAG .
+# Single architecture build (amd64)
+#docker build -t dockerregistry.metaways.net/tine20/tine20-broadcasthub:$CI_COMMIT_TAG -t registry.metaways.net/tine/broadcasthub:$CI_COMMIT_TAG -t docker.io/tinegroupware/broadcasthub:$CI_COMMIT_TAG .
+
+
+#
+# Multi-architecture build by QEMU emulation in the kernel
+#
+
+# Install buildx, not included in docker:20.10.11
+# See https://docs.docker.com/build/buildx/install/#linux-packages
+apk update
+apk add curl
+latest=$(curl -I https://github.com/docker/buildx/releases/latest | grep -i ^Location: | cut -d: -f2- | sed 's/^ *\(.*\).*/\1/' | sed 's#^https://github\.com/docker/buildx/releases/tag/\(v.\+\)$#\1#'  | tr -d '\r')
+echo "Fetching buildx from https://github.com/docker/buildx/releases/download/${latest}/buildx-${latest}.linux-amd64"
+wget "https://github.com/docker/buildx/releases/download/${latest}/buildx-${latest}.linux-amd64"
+chmod +x buildx-${latest}.linux-amd64
+mkdir -p /usr/libexec/docker/cli-plugins/
+mv buildx-${latest}.linux-amd64 /usr/libexec/docker/cli-plugins/docker-buildx
+
+
+# See https://docs.docker.com/build/building/multi-platform/
+
+# Install binfmt_misc handler for access to QEMU
+docker run --privileged --rm tonistiigi/binfmt --install all
+
+# Create and use custom builder instance
+docker buildx create --name container --driver=docker-container --bootstrap
+docker buildx use container
+
+# Print information about building capabilties
+docker context ls
+docker buildx version
+docker buildx ls
+
+# Build is done along with push in ci_publish_image.sh
--- build/ci_publish_image.sh
+++ build/ci_publish_image.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+# Singel architecture pushes (amd64)
+# docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
+# docker push dockerregistry.metaways.net/tine20/tine20-broadcasthub:$CI_COMMIT_TAG
+#
+# docker login -u $MW_MW_REGISTRY_USER -p $MW_MW_REGISTRY_PASSWORD registry.metaways.net
+# docker push registry.metaways.net/tine/broadcasthub:$CI_COMMIT_TAG
+#
+# docker login -u $MW_DOCKER_HUB_USER -p $MW_DOCKER_HUB_PASSWORD docker.io
+# docker push docker.io/tinegroupware/broadcasthub:$CI_COMMIT_TAG
+
+#
+# Multi-architecture builds and pushes
+#
+
+# Log into each registry
+# dockerregistry.metaways.net
 docker login -u $CI_REGISTRY_USER -p $CI_REGISTRY_PASSWORD $CI_REGISTRY
-docker push dockerregistry.metaways.net/tine20/tine20-broadcasthub:$CI_COMMIT_TAG
-
+# registry.metaways.net
 docker login -u $MW_MW_REGISTRY_USER -p $MW_MW_REGISTRY_PASSWORD registry.metaways.net
-docker push registry.metaways.net/tine/broadcasthub:$CI_COMMIT_TAG
-
+# docker.io
 docker login -u $MW_DOCKER_HUB_USER -p $MW_DOCKER_HUB_PASSWORD docker.io
-docker push docker.io/tinegroupware/broadcasthub:$CI_COMMIT_TAG
+
+# Build and push to registries
+docker buildx build --push --platform linux/amd64,linux/arm64/v8 -t dockerregistry.metaways.net/tine20/tine20-broadcasthub:$CI_COMMIT_TAG -t registry.metaways.net/tine/broadcasthub:$CI_COMMIT_TAG -t docker.io/tinegroupware/broadcasthub:$CI_COMMIT_TAG .
